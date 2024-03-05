e68be859a6faf09007e778d39427205b163586e0
zeorphix <tieue45@gmail.com>
2024-03-05 03:34:00 -0500

    file creation

---
+++ problem1.cpp
@@ -0,0 +1,4 @@
+// Evan Tieu
+// Programming Assignment 3
+// Problem 1: The Birthday Presents Party
+// COP 4520, Spring 2024
---
+++ problem2.cpp
@@ -0,0 +1,4 @@
+// Evan Tieu
+// Programming Assignment 3
+// Problem 2: Atmospheric Temperature Reading Module
+// COP 4520, Spring 2024
