7567dace56c7c44cf014a620cb95c7545119f2ce
zeorphix <tieue45@gmail.com>
2024-03-30 01:49:31 -0400

    thread creation

--- problem2.cpp
+++ problem2.cpp
@@ -7,7 +7,23 @@
 #include <thread>
 #include <mutex>
 
+const int NUM_SENSORS = 8;
+const int NUM_READINGS_PER_HOUR = 60;
+const int NUM_MINUTES_PER_HOUR = 60;
+
+std::mutex mtx;
+
 int main(void)
 {
+    using namespace std;
+
+    vector<thread> sensorThreads;
+
+    for (int i = 0; i < NUM_SENSORS; ++i)
+        sensorThreads.push_back(thread(generateTemp), i + 1));
+
+    for (auto& sensor : sensors)
+        sensor.join();
+    
     return 0;
 }
