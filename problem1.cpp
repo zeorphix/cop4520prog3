6f993567efa8b176c41d3e1138d1c30c7386284c
zeorphix <tieue45@gmail.com>
2024-03-30 00:23:09 -0400

    lib

--- problem1.cpp
+++ problem1.cpp
@@ -3,6 +3,10 @@
 // Problem 1: The Birthday Presents Party
 // COP 4520, Spring 2024
 
+#include <iostream>
+#include <thread>
+#include <mutex>
+
 int main(void)
 {
     return 0;
--- problem2.cpp
+++ problem2.cpp
@@ -3,6 +3,11 @@
 // Problem 2: Atmospheric Temperature Reading Module
 // COP 4520, Spring 2024
 
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+
 int main(void)
 {
     return 0;
